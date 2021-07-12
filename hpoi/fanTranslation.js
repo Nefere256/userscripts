f172afc6faddc5117549f315846685c499bb5da5
Nefere256 <46942248+Nefere256@users.noreply.github.com>
2021-07-12 22:12:06 +0200

    #5 global search page full translation

--- hpoi/fanTranslation.js
+++ hpoi/fanTranslation.js
@@ -3,7 +3,7 @@
 // @namespace https://takkkane.tumblr.com/scripts/hpoiTranslation
 // @supportURL     https://twitter.com/TaxDelusion
 // @description A script that translates text on Hpoi website to easily navigate
-// @version  0.1.5
+// @version  0.1.6
 // @downloadURL	https://raw.githubusercontent.com/Nefere256/userscripts/master/hpoi/fanTranslation.js
 // @include  https://www.hpoi.net/*
 // @require  https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
@@ -75,11 +75,11 @@ const TRANSLATIONS = {
     },
     'nav_top_search_drop_list' : {
       '全部周边' : 'All',
-      '角色' : 'Role',
+      '角色' : 'Character',
       '作品' : 'Series',
       '系列' : 'Line',
       '人物' : 'Person',
-      '厂商' : 'Maker',
+      '厂商' : 'Company',
       '用户' : 'User',
     },
     'nav_top_search_drop_list_default' : {
@@ -93,6 +93,7 @@ const TRANSLATIONS = {
     'profile_desc' : {
 			'还没有信仰_(:з」∠)_' : 'not much to say _(:з」∠)_',
     },
+    /* HOME PAGE - FEEDS */
     'home_action_type_filter' : {
       '全部' : 'All',
       '情报' : 'Info',
@@ -139,6 +140,7 @@ const TRANSLATIONS = {
       '相册' : 'Album',
       '作品' : 'Series',
     },
+    /* TEM PAGE */
     'item_prop' : {
       '名称：' : 'Name:',
       '别名：' : 'Alias:',
@@ -163,6 +165,7 @@ const TRANSLATIONS = {
       '尺寸：': 'Size:',
       '版权：' : 'Copyrights:',
 			'原型协力：' : 'Producer:',
+      '官网：' : 'Website:',
       '官方链接：' : 'Off. links:' ,
       '外部链接：' : 'Ref. links:',
     },
@@ -357,6 +360,61 @@ const TRANSLATIONS = {
       '确定' : 'OK',
     },
     
+    /* Global search */
+    'search_global_advanced_search_button' : {
+      '高级检索' : 'Advanced',
+    },
+    
+    'search_global_main_nav' : {
+      '周边' : 'Items',
+      '图片' : 'Pictures',
+			'相册' : 'Albums',
+      '角色' : 'Chars',
+      '作品' : 'Series',
+      '系列' : 'Lines',
+      '人物' : 'Persons',
+      '厂商' : 'Makers',
+      '用户' : 'Users',
+    },
+    'search_global_tags_additional' : {
+      '周边系列' : 'Line', // text for global search tags
+      '手办相册' : 'Figure album',
+      '动漫周边' : 'Merch',
+    },
+    'search_global_option_title' : {
+      '综合排序' : 'Complex search',
+      '全部周边' : 'All types',
+      '全部手办' : 'All figures',
+      '全部动漫模型' : 'All anime models',
+      '全部真实模型' : 'All real models',
+      '全部毛绒布偶' : 'All plushies',
+      '全部Doll娃娃' : 'All dolls',
+      '立牌/摆件' : 'Character stand',
+      '全部厂商' : 'All companies',
+      '不限年份' : 'Any year',
+    },
+    'search_global_option_sort' : {
+      '相关度最高' : 'Most revelant',
+      '最热门' : 'Popular first',
+      '最新添加' : 'Newly added',
+      '最晚发售' : 'Latest released',
+      '评分最高' : 'Best rated',
+    },
+    'search_global_results_none' : {
+      '什么也没找到, 可以尝试按"相关度最高"排序看看' : 'Couldn\'t find anything, try to sort by \'Most revelant\'.',
+    },
+    'search_global_page_ibox' : {
+      '说明' : 'How to use',
+      '大家在找' : 'Most popular',
+    },
+    'search_global_ibox_description' : {
+      '1、尽量用日语原文作关键字，因为很多还没翻译，或者翻译不标准' : '1. If possible, use Japanese names to search over Chinese ones, as not all items are translated, or Chinese names are not common.',
+      '2、分类、排序、样式都还要完善，先用着吧_(:з」∠)_' : '2. Sorting and filtering by types and all clasifications are your friends, use them first _(:з」∠)_',
+      '3、如果要按性质(如可脱)查找，也可以看看' : '3. If you want more detailed filtering (like for figures with alternate parts), take a look at ',
+      '高级检索' : 'an advanced search.',
+    
+    },
+    
     /* Encyclopedia */
     'encyclopedia_nav' : {
       '概述' : 'Overview',
@@ -375,6 +433,8 @@ const TRANSLATIONS = {
       '别名:' : 'Aliases:',
       '官网:' : 'Website:',
       '官方网站:' : 'Official website:',
+      '官方微博:' : 'Official Weibo:',
+      '推特:' : 'Twitter',
       
       '成立时间:' : 'Founded date:',
       '所在地:' : 'Location:',
@@ -406,6 +466,11 @@ const TRANSLATIONS = {
       '发行日期:' : 'Released:',
       '开发:' : 'Developed:',
     },
+    'encyclopedia_series_types' : {
+      '动画' : 'Video',
+      '游戏' : 'Game',
+      '其它' : 'Other',
+    },
     'encyclopedia_items_section' : {
       '最新作品' : 'Latest items',
       '关联手办' : 'Related figures',
@@ -415,24 +480,13 @@ const TRANSLATIONS = {
       '她参与的手办' : 'Figures worked on',
       '他参与的手办' : 'Figures worked on',
     },
-    /*
-    
-  系列 共13个 => Lines, total of 13
-  系列 共4个 => Lines, total of 4
-  制作周边 共596个, 评分4.44 => Made works, total of 596, rating 4.44
-  制作周边 共220个, 评分4.33 => Made works, total of 220, rating 4.33
-  发行周边 共297个 => Distributed works, total of 220
-  发行周边 共1个 => Distributed works, total of 1
-  她参与的手办 共110个, 评分4.43 => Figures worked on, total of 110, rating 4.43
-  他参与的手办 共288个, 评分4.38 => Figures worked on, total of 288, rating 4.38
-    */
     'encyclopedia_items_more' : {
       '查看更多' : 'see more',
     },
     
     
 // types (filter)
-  	'x_generic_all' : {
+    'x_generic_all' : {
       '全部' : 'all',
       '不限' : 'all', // actualy - non required
       '周边' : 'all',
@@ -441,22 +495,73 @@ const TRANSLATIONS = {
     '其它' : 'other',
   
   /* SORTED DIC */
-  'x_item_types' : {
+    'x_item_types' : {
       '手办' : 'Figure',
       '动漫模型' : 'Anime model',
       '真实模型' : 'Real model',
       '毛绒布偶' : 'Plushie',
       'Doll娃娃' : 'Doll',
-    '动漫周边' : 'Merch',
+      '动漫周边' : 'Merch',
     }, 
-  'x_item_types_plural' : {
+    'x_item_types_plural' : {
       '手办' : 'Figures',
       '动漫模型' : 'Anime models',
       '真实模型' : 'Real models',
       '毛绒布偶' : 'Plushies',
       'Doll娃娃' : 'Dolls',
     },
-},
+    'x_subtypes_figures' : {
+			'比例人形' : 'Scale figure',
+      'Q版人形' : 'Chibi figure',
+      '盒蛋/扭蛋' : 'Blind box/gacha',
+      '怪兽/机械' : 'Monster/mecha',
+      '仿真人物' : 'Real person',
+      '配件' : 'Accessory',
+      '场景' : 'Diorama',
+    },
+    'x_subtypes_anime_models' : {
+      '机甲-拼装' : 'Mecha - to assembly',
+      '机甲-完成品' : 'Mecha - completed',
+      '机甲-配件' : 'Mecha - accessory',
+      '特摄英雄' : 'Tokusatsu hero',
+      '特摄怪兽' : 'Tokusatsu monster',
+      '特摄配件' : 'Tokusatsu accessory',
+      '扭蛋/玩具' : 'Gacha/toy',
+      '驱动模型(四驱车)' : 'Vechicle (4 wheels)',
+      '驱动模型配件' : 'Vechicle accessory',
+    },
+    'x_subtypes_real_models' : {
+      '拼装' : 'To assembly',
+      '完成品' : 'Completed',
+      '人形' : 'Person',
+      '场景配件' : 'Diorama accessory',
+      '工具材料' : 'Tool',
+    },
+    'x_subtypes_plushies' : {
+      '人形' : 'Human',
+      '拟人形' : 'Anthropomorphic',
+      '动植物' : 'Flora and fauna',
+    },
+    'x_subtypes_dolls' : {
+      'Doll完成品' : 'Complete doll',
+		},
+    'x_subtypes_merch' : {
+      '立牌/摆件' : 'Character stand',
+      '箱包鞋服' : 'Bags and shoes',
+      '穿戴配饰' : 'Clothes',
+      '徽章/挂件' : 'Badges / keychains',
+      '海报/装饰画' : 'Artworks',
+      '抱枕/家纺' : 'Daily use / stationery',
+      '日用/文具' : 'Linens',
+      '数码/配件' : 'Digital / accesories',
+      '拼图纸膜' : 'Papercraft',
+      '其它周边' : 'Other merch',
+    },
+    'x_other' : {
+    	'其它' : 'Other',
+      '其他' : 'Other',
+    },
+  }
 };
 
   
@@ -500,11 +605,26 @@ const PLACES = {
   'search_modal_window_header' : '#selectDateModal .modal-header > h4.modal-title',
   'search_modal_window_body' : "#selectDateModal .modal-body label",
   'search_modal_window_footer' : "#selectDateModal .modal-footer > button",
+  /* global search */
+  'search_global_advanced_search_button' : 'div.page-search-btn ~ a.btn-link',
+  'search_global_main_nav' : 'div.taobao-nav > div',
+  'search_global_option_title' : '.search-option-nav > div:nth-of-type(1)',
+  'search_global_option_nav' : '.search-option-nav',
+  'search_global_option_sort' : '.search-option-nav:nth-of-type(3) > div:not(:nth-of-type(1))',
+  'search_global_option_item_types' : '.search-option-nav:nth-of-type(4) > div:not(:nth-of-type(1))',
+  'search_global_option_item_type_active' : '.search-option-nav:nth-of-type(4) > div.active',
+  'search_global_option_item_subtypes' : '.search-option-nav:nth-of-type(5) > div:not(:nth-of-type(1))',
+  'search_global_result_tags' : '#result-content > .media-list .ibox-content > .media-body > div > span.label-tag',
+  'search_global_users_stats' : '#result-content .user-i-box .row > div',
+  'search_global_results_none' : 'div#result-content > div:not(.pull-right)',
+  'search_global_page_ibox'	: '.ibox > .ibox-title > h5',
+  'search_global_ibox_description' : '.ibox:first > .ibox-content  *',
   /* ENCYCLOPEDIA */
   'encyclopedia_nav' : 'nav.navbar-inner > ul > li > a',
   'encyclopedia_infobox_props' : 'table.info-box td.info-box-left',
   'encyclopedia_items_more' : '.subfield a',
   'encyclopedia_items_section' : '.subfield span',
+  
   
   /* OTHER */
   'item_prop'	: '.hpoi-ibox-content > .infoList-box > .hpoi-infoList-item > span',
@@ -525,12 +645,15 @@ const PLACES = {
 * methodType - 'item_first' - replace the exact string appearing in itemInQuestion results
 		- 'dic_first' - check all keys listed in subDictionaries and replace them with translations stored in values
 */
-const doTranslation = function(itemInQuestion, subDictionaries = [], methodType = 'item_first') {
+const doTranslation = function(itemInQuestion, subDictionaries = [], methodType = 'item_first', elementsInQuestion) {
   if (subDictionaries.length) {
     methodType = 'dic_first';
   }
-  
-  let items = $(PLACES[itemInQuestion]);
+  let items;
+  if(!elementsInQuestion)
+  	items = $(PLACES[itemInQuestion]);
+  else
+    items = elementsInQuestion;
   
   let textItems = items.contents().filter(function() {
     return this.nodeType === Node.TEXT_NODE;
@@ -757,6 +880,57 @@ $(document).ready(function () {
     doTranslation('search_modal_window_header');
     doTranslation('search_modal_window_body');
     doTranslation('search_modal_window_footer');
+    
+  }
+  if (window.location.pathname.includes('/search')) {
+    doTranslation('search_global_advanced_search_button');
+    doTranslation('search_global_main_nav');
+    
+    // search
+    var nav_option_groups = $(PLACES['search_global_option_nav']);
+    doTranslation('search_global_option_title');
+    doTranslation('search_global_option_sort');
+    doTranslation('search_global_option_item_types', ['x_item_types_plural','x_other']);
+    
+    let activeSubtype = $(PLACES['search_global_option_item_type_active']);
+    let dicToTranlateSubtypes = "";
+    if (activeSubtype.length) {
+      var subtype = activeSubtype[0].innerText.trim();
+      switch (subtype) {
+          case 'All types':
+          break;
+          case 'Figures' : 
+          dicToTranlateSubtypes = 'x_subtypes_figures';
+          break;
+          case 'Anime models' : 
+          dicToTranlateSubtypes = 'x_subtypes_anime_models';
+          break;
+          case 'Real models' : 
+          dicToTranlateSubtypes = 'x_subtypes_real_models';
+          break;
+          case 'Plushies' : 
+          dicToTranlateSubtypes = 'x_subtypes_plushies';
+          break;
+          case 'Dolls' : 
+          dicToTranlateSubtypes = 'x_subtypes_dolls';
+          break;
+        case 'Other' :
+          dicToTranlateSubtypes = 'x_subtypes_merch'
+          break;
+      }
+    } else {
+      dicToTranlateSubtypes = 'x_subtypes_merch';
+    }
+    if(dicToTranlateSubtypes != "") {
+      doTranslation('search_global_option_item_subtypes', [dicToTranlateSubtypes, 'x_other']);
+    }
+
+    doTranslation('search_global_result_tags', ['search_global_tags_additional','nav_top_search_drop_list',
+                                                'encyclopedia_series_types', 'home_image_type_name']);
+    doTranslation('search_global_users_stats', ['profile_stats']);
+    doTranslation('search_global_results_none');
+    doTranslation('search_global_page_ibox');
+    doTranslation('search_global_ibox_description');
   }
   
   doTranslation('encyclopedia_nav');
@@ -766,7 +940,6 @@ $(document).ready(function () {
     translateEncyclopediaItemsHeader(element, 'encyclopedia_items_section');
   });
   
- // doTranslation('encyclopedia_items_section');
   doTranslation('encyclopedia_items_more');
   
   
@@ -830,7 +1003,20 @@ $(document).ready(function () {
     	testTranslationMap('search_modal_window_header');
     	testTranslationMap('search_modal_window_body');
     	testTranslationMap('search_modal_window_footer');
+      
+      
     }
+    if (window.location.pathname.includes('/search')) {
+      testTranslationMap('search_global_advanced_search_button');
+      testTranslationMap('search_global_main_nav');
+      testTranslationMapForDic('search_global_result_tags', ['search_global_tags_additional','nav_top_search_drop_list',
+                                                'encyclopedia_series_types', 'home_image_type_name']);
+      testTranslationMapForDic('search_global_users_stats', ['profile_stats']);
+      testTranslationMap('search_global_results_none');
+    	testTranslationMap('search_global_page_ibox');
+      testTranslationMap('search_global_ibox_description');
+  	}
+    
     testTranslationMap('encyclopedia_nav');
     testTranslationMap('encyclopedia_infobox_props');
  		testTranslationMapForDic('encyclopedia_items_section', ['encyclopedia_items_section']);

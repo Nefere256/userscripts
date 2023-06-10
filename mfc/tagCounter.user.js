4fbdbafe4b728360cf2d7bfa5cbf3dcab9a77b1a
Nefere256 <blog.delusion@gmail.com>
2023-06-10 22:08:46 +0200

    init mfc tagCounter script

---
+++ mfc/tagCounter.user.js
@@ -0,0 +1,94 @@
+// ==UserScript==
+// @name         Tag counter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  add tags counter to list of entries. Great for MFC editors!
+// @author       Nefere
+// @match        https://myfigurecollection.net/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=myfigurecollection.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var TAG_CLASSNAME = "us-tag";
+    var FAKE_CLASS_PLACEHOLDER = "what-i-was-looking-for";
+	var REQUEST_DELAY = 1000;
+	
+	function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	};
+    function addStyles() {
+        $("<style>")
+        .prop("type", "text/css")
+        .html("\
+        .item-icon ." + TAG_CLASSNAME + " {\
+            position: absolute;\
+            display: block;\
+            right: 1px;\
+            bottom: 1px;\
+            height: 16px;\
+            padding: 0 4px;\
+            font-weight: 700;\
+            color: gold;\
+            background-color: darkgreen\
+          }")
+        .appendTo("head");
+    };
+    function getEntryContainers() {
+        if (window.location.pathname.includes("/entry/")) {
+            var result = $("#wide .results .result");
+            return result;
+        }
+        console.log("unsupported getEntryContainers");
+        return $(FAKE_CLASS_PLACEHOLDER);
+    };
+    function getItemsFromContainer(entryContainer) {
+        var icons = $(entryContainer).find(".item-icons .item-icon");
+        if (icons.length > 0) {
+            return icons;
+        }
+        console.log("unsupported getItemsFromContainer");
+        return $(FAKE_CLASS_PLACEHOLDER);
+    };
+	function getTagCounterFromHtml(html){
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(html, 'text/html');
+        var tagCounterNode = doc.querySelector('.tbx-target-TAGS .count');
+        return tagCounterNode.textContent;
+	};
+	function addTagCounterToSearchResult(itemLinkElement, countOfTags) {
+        var tagElement = document.createElement("span");
+        tagElement.setAttribute("class", TAG_CLASSNAME);
+        tagElement.textContent = countOfTags;
+        itemLinkElement.appendChild(tagElement);
+	};
+	async function main(){
+		var queue = [];
+		addStyles();
+		var entryContainers = getEntryContainers();
+		entryContainers.each(function(i, entryContainer) {
+			var itemsElements = getItemsFromContainer(entryContainer);
+			itemsElements.each(function(i, itemElement) {
+				queue.push(itemElement);
+			});
+		});
+		for(var itemElement of queue) {
+			var itemLinkElement = itemElement.firstChild;
+			var entryLink = itemLinkElement.getAttribute("href");
+			
+			fetch(entryLink).then(function (response) {
+				return response.text();
+			}).then(function (html) {
+				var countOfTags = getTagCounterFromHtml(html);
+				addTagCounterToSearchResult(itemLinkElement, countOfTags);
+			}).catch(function (err) {
+				console.warn('Something went wrong.', err);
+			});
+			await sleep(REQUEST_DELAY);
+			
+		}
+	};
+	
+	main();
+})();
